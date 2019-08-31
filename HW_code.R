ba5481a83ae4a9b2419c888578892cf5e0dd31d5
Shao <ychen247@ncsu.edu>
2019-08-31 12:36:26 -0400

    Update HW_code.R

--- HW_code.R
+++ HW_code.R
@@ -31,33 +31,58 @@ checkstring <- function(v, naVal = NULL) {
   v_unique2 <- v_unique[! v_unique %in% naVal]
   # count number of unique values and check whether all values are integers
   if ( length(unique(v_unique2)) > 2L || 
-       any(as.integer(v_unique2) != v_unique2) ) "nominal" else "ordinal"
+       any(as.integer(v_unique2) != v_unique2) ) "nom" else "ord"
 }
 
 #     Gen to null vector
-var <- c()
+list=colnames(insurance_t) 
 type <- c()
 
 #     For loop to indentify type of each value
 #     Not the final version, this code using the fact that numeric variables put in the begining in dataset
 for (i in colnames(insurance_t)){
-  var=append(var,i)
   type=append(type,checkBinaryTrait(insurance_t[[i]]))}
 
 #     Cheating code to add value manually   
-var=append(var,"RES")
-type=append(type,"nominal")
-type=append(type,"nominal")
+type=append(type,"nom")
+type=append(type,"nom")
 
-#     Transform vector to dataframe
-datasummary=data.frame(var,type)
+datasummary=data.frame(list,type)
 #################################################################
 
 
-# 2.1 Binary variables' odds ratios
-logit.model_2 <- glm(INS ~ DDA, data = insurance_t, family = binomial(link = "logit"))
-summary(logit.model_2)
-# 2.2 Rank these odds ratios by magnitude.
+# 2   Binary variables' odds ratios
+
+# 2.1 Gen binary set of dataframe "binvar"
+binvar=subset(datasummary,type=="bin")
+#     remove dependent var "INS"
+binvar=subset(binvar, list!="INS")
+
+# 2.2 Gen loop to run logit model and get p-values and odd ratio
+
+#     Gen list of dependent variable and independent variables
+dep_vars <- c("INS") 
+ind_vars <- droplevels(binvar$list)
+# pair with dep_vars:
+var_comb <- expand.grid(dep_vars, ind_vars ) 
+# formulas for all combinations
+formula_vec <- sprintf("%s ~ %s", var_comb$Var1, var_comb$Var2)
+
+#     loopppppp for gen list of "pvalue" and "odd"
+pvalue <- c()
+odd <- c()
+for (model in formula_vec){
+  logit.model<- glm(model, data = insurance_t, family = binomial(link = "logit"))
+  pvalue=append(pvalue,coef(summary(logit.model))[2,4])
+  odd=append(odd,exp(coef(logit.model))[2])
+}
+#     Combine list to dataframe
+binvar["pvalue"] <- NA
+binvar$pvalue <- pvalue
+binvar["odd"] <- NA
+binvar$odd <- odd
+
+# 2.3 Rank these odds ratios by magnitude.
 #################################################################
 # 3   Continuous variables' linearity assumption
 #################################################################
