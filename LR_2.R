f66cc081482254f5400e3c9f6326376188bc254d
陳昱卲 <shao@ip150-55.vpnip.cc.ntu.edu.tw>
2019-09-12 15:54:57 -0400

    Update LR_2.R

--- LR_2.R
+++ LR_2.R
@@ -2,6 +2,8 @@ library(tidyr)
 library(dplyr)
 library(ggplot2)
 
+
+# Step 0: Looking at missing pattern
 missing.values <- insurance_t_bin %>%
   gather(key = "key", value = "val") %>%
   mutate(is.missing = is.na(val)) %>%
@@ -58,18 +60,23 @@ row.plot <- insurance_t_bin %>%
   coord_flip()
 
 row.plot
+# Step 0 wrapup: VAR with missing value (INV, CCPURC, CC), HMOWN
 
 
-### VAR with missing value (INV, CCPURC, CC), HMOWN
+# Step 1: Recode all NA to missing
 insurance_t_bin[is.na(insurance_t_bin)] <- "Missing"
 
-### Two Way Table: CASHBK, MMCRED, CCPURC have complete or quasi-separation concerns
+
+# Step 2: Loop for all two-way tables
 for (i in seq_along(insurance_t_bin)){
   print(colnames(insurance_t_bin)[i])
   print(table(insurance_t_bin[[i]],insurance_t_bin[["INS"]]))  
 }
+# Step 2 wrapup: Two Way Table: CASHBK, MMCRED, CCPURC have complete or quasi-separation concerns
 
-###
+
+
+# Step 3: Recode vars have complete or quasi-separation concerns
 insurance_t_bin$CASHBK[insurance_t_bin$CASHBK==2]=1
 table(insurance_t_bin[["CASHBK"]],insurance_t_bin[["INS"]])
 
@@ -82,7 +89,9 @@ insurance_t_bin$CCPURC[insurance_t_bin$CCPURC==4]=2
 table(insurance_t_bin[["CCPURC"]],insurance_t_bin[["INS"]])
 
 
-# Build Model
+# This is optional
+# Look at whether variables are binary or catagorical
+# This effect whether put factor() or not
 checktype <- function(v, naVal = NULL) { 
   # get unique values
   v_unique <- unique(v)
@@ -102,15 +111,20 @@ for (i in list){
 vartype=data.frame(list,type)
 
 
+
+# Step 4: Define full.model
 full.model <- glm(INS~
 DDA+ CASHBK+ DIRDEP+ NSF+ ATM+ CD+ IRA+ LOC+ ILS+MM+MTG+SDB+MOVED+INAREA+ NSFAMT_Bin+ IRABAL_Bin+ ILSBAL_Bin+ MMBAL_Bin+ LORES_Bin
 +factor(INV)+factor(MMCRED)+factor(CC)+factor(CCPURC)+factor(HMOWN)+factor(BRANCH)+factor(RES)+factor(DDABAL_Bin)+factor(ACCTAGE_Bin)+factor(DEPAMT_Bin)+factor(CHECKS_Bin)+factor(PHONE_Bin)+factor(TELLER_Bin)+factor(SAVBAL_Bin)+factor(ATMAMT_Bin)+factor(POS_Bin)+factor(POSAMT_Bin)+factor(CDBAL_Bin)+factor(LOCBAL_Bin)+factor(INVBAL_Bin)+factor(MTGBAL_Bin)+factor(CCBAL_Bin)+factor(INCOME_Bin)+factor(HMVAL_Bin)+factor(AGE_Bin)+factor(CRSCORE_Bin),
 data = insurance_t_bin, family = binomial(link = "logit"))
 
+# Set alpha level to 0.002
 qchisq(0.002,1,lower.tail=FALSE)
 
+# Step 5: Main model selection
 back.model <- step(full.model, direction = "backward", k=9.549536)
 
+# Step 6: Model with interaction
 int.model <- step(back.model, scope = . ~ .^2, direction = 'forward', k=9.549536)
 
 
