392e795e90e00b3d892b1b1bcfed1f2d5defecea
陳昱卲 <shao@chenyushaode-MacBook-Pro.local>
2019-10-07 19:05:14 -0400

    Update TS_4.R

--- TS_4.R
+++ TS_4.R
@@ -44,6 +44,7 @@ test=subset(ts,start=length(ts)-5)
 # Use STL decomposition to find patterns #
 decomp_stl <- stl(training, s.window = 7)
 plot(decomp_stl)
+
 # Find both season and trend component 
 
 
@@ -59,25 +60,31 @@ model  <- tslm(training ~ month)
 tsdisplay(residuals(model))
 ts_r=residuals(model)
 
+decomp_stl <- stl(ts_r, s.window = 7)
+plot(decomp_stl)
+
 # Testing stationary #
 adf.test(ts_r, alternative = "stationary", k=0)
 adf.test(ts_r, alternative = "stationary", k=1)
 adf.test(ts_r, alternative = "stationary", k=2)
 
 # Fit Trend #
-model  <- tslm(training ~ trend+month, lambda=1)
+model  <- tslm(training ~ trend+I(trend^2)+season)
 tsdisplay(residuals(model))
 ts_r=residuals(model)
 forcast_ts <- forecast(model,data.frame(month=I(seasonaldummy(training,6))))$mean
+forcast_ts <- forecast(model,h=6,level = 0)$mean
+
+
 
 # Auto select #
 auto.arima(ts_r)
 
 # Results #
-mod1<-sarima(ts_r, 1,0,0,1,0,0,12)
+mod1<-sarima(ts_r, 1,0,0,0,0,0,12)
 
 # Forcast #
-mod1 <- Arima(ts_r, order=c(1,0,0), seasonal=c(1,0,0), method="ML")
+mod1 <- Arima(ts_r, order=c(1,0,0), seasonal=c(0,0,0), method="ML")
 forcast_r=forecast(mod1, h = 6)$mean
 forcast=forcast_ts+forcast_r
 
@@ -96,4 +103,55 @@ ggplot()+
   scale_x_yearmon()+
   labs(title =  'Prediction versus Actual in Validation Set',y = "Monthly Average PM2.5",x = "Year")+
   theme_minimal(base_size = 10)+
-  theme(plot.title = element_text(hjust = 0.5),plot.subtitle = element_text(hjust= 0.5),legend.title=element_blank(),legend.position="bottom")
+  theme(plot.title = element_text(hjust = 0.5),plot.subtitle = element_text(hjust= 0.5),legend.title=element_blank(),legend.position="bottom",text = element_text(size=12))
+
+error=df$Actual-df$Predicted
+MAE=mean(abs(error))
+MAPE=mean(abs(error)/abs(test))
+print(MAPE)
+
+
+
+#visualization
+#seasonal component
+seasonal   <- decomp_stl$time.series[,1]
+df=data.frame(Seasonal=as.matrix(seasonal), date=as.yearmon(time(seasonal)))
+
+ggplot()+
+  geom_line(data=df,aes(y=Seasonal,x= date,color="Seasonal Component"),size=1 )+
+  scale_color_manual(values = c("Seasonal Component" = "coral3"))+
+  scale_x_yearmon()+
+  labs(title =  'Prediction versus Actual in Validation Set',y = "Monthly Average PM2.5",x = "Time (Month-Year)")+
+  theme_minimal(base_size = 10)+
+  theme(plot.title = element_text(hjust = 0.5),plot.subtitle = element_text(hjust= 0.5),legend.title=element_blank(),legend.position="bottom",text = element_text(size=12,family="sans"))
+
+#forecasting
+#import forecast_csv
+df=data.frame(Actual=as.matrix(test), date=as.yearmon(time(test)))
+df$Predicted=forecast_csv$FORECAST
+
+ggplot()+
+  geom_line(data=df,aes(y=Actual,x= date,color="Actual"),size=1 )+
+  geom_line(data=df,aes(y=Predicted,x= date,color="Forecasted"),size=1)+ 
+  scale_color_manual(values = c("Forecasted" = "goldenrod1", "Actual" = "darkcyan"))+
+  scale_x_yearmon()+
+  labs(title =  'Prediction versus Actual in Validation Set',y = "Monthly Average PM2.5",x = "Time (Month-Year)")+
+  theme_minimal(base_size = 10)+
+  theme(plot.title = element_text(hjust = 0.5),plot.subtitle = element_text(hjust= 0.5),legend.title=element_blank(),legend.position="bottom",text = element_text(size=12,family="sans"))
+
+
+#trend fitting
+#import trend
+df=data.frame(Actual=as.matrix(training), date=as.yearmon(time(training)))
+df$Residuals=trend$Residuals
+df$Trend=trend$Trend
+
+ggplot()+
+  geom_line(data=df,aes(y=Trend,x= date,color="Trend"),size=1 )+
+  geom_line(data=df,aes(y=Residuals,x= date,color="Residuals (after removing seasonality)"),size=1)+ 
+  scale_color_manual(values = c("Trend" = "#1995AD", "Residuals (after removing seasonality)" = "#A1D6E2"))+
+  scale_x_yearmon()+
+  labs(title =  'Prediction versus Actual in Validation Set',y = "Monthly Average PM2.5",x = "Time (Month-Year)")+
+  theme_minimal(base_size = 10)+
+  theme(plot.title = element_text(hjust = 0.5),plot.subtitle = element_text(hjust= 0.5),legend.title=element_blank(),legend.position="bottom",text = element_text(size=12,family="sans"))
+
